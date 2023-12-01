3e97491532869cfe6e3c1ae007ba64a343131437
Alabi Ayobami <92615787+ThatLinuxGuyYouKnow@users.noreply.github.com>
2023-12-01 23:40:29 +0100

    Update application_job.rb

--- app/jobs/application_job.rb
+++ app/jobs/application_job.rb
@@ -1,6 +1,6 @@
 class ApplicationJob < ActiveJob::Bas
   # Automatically retry jobs that encountered a deadlock
-  # retry_on ActiveRecord::Deadlocked
+  # retry_on ActiveRecord::Deadlocke
 
   # Most jobs are safe to ignore if the underlying records are no longer available
   # discard_on ActiveJob::DeserializationError
